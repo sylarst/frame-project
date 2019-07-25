b8f2e2115f757491825e37867728eb3532775ab2
sylay <arcilis@163.com>
2019-07-25 09:56:09 +0800

    ~

--- applymodule/src/main/java/com/age/mac/applymodule/ApplyActivity.java
+++ applymodule/src/main/java/com/age/mac/applymodule/ApplyActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
-import com.age.mac.baselibrary.utils.ArouterConfig;
+import com.age.mac.baselibrary.base.ArouterConfig;
 import com.alibaba.android.arouter.facade.annotation.Route;
 
 /**
